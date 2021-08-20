34397ff617b43662e6c2801d30a31a7aacd208f7
saintravi <Dudah4@gmail.com>
2021-08-20 01:09:57 -0300

    att desafio melhor amg do pablo

--- Java/Solução de Problemas com Java/MelhorAmigoPablo.java
+++ Java/Solução de Problemas com Java/MelhorAmigoPablo.java
@@ -44,7 +44,7 @@ public class MelhorAmigoPablo {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String linha = br.readLine();
         List<Participante> participantes = new ArrayList<>();
-    
+
         while (!linha.equals("FIM")) {
             var participante = new Participante();
             var entrada = linha.split(" ");
@@ -55,26 +55,26 @@ public class MelhorAmigoPablo {
                     .filter(p -> p.getNome().equals(participante.getNome()))
                     .findFirst()
                     .orElse(null);
-                    
+
             if(participanteExistente == null) {
                 participantes.add(participante);
             }
             linha = br.readLine();
         }
-    
+
         var participantesOrdenados = participantes.stream().sorted(Comparator
                 .comparing(Participante::isAmigo).reversed()
                 .thenComparing(Participante::getNome)).collect(Collectors.toList());
-    
+
         participantesOrdenados.forEach(p -> {
-            if(!p.getNome().equals("Abhay")) {
+            if(!p.getNome().equals(participantes)) {
                 System.out.println(p.getNome());
             }
         });
-    
+
         List<Participante> amigos = participantesOrdenados.stream().filter(Participante::isAmigo).collect(Collectors.toList());
         Participante escolhido = null;
-    
+
         for (Participante p : amigos) {
             if(escolhido == null) {
                 escolhido = p;
@@ -85,41 +85,41 @@ public class MelhorAmigoPablo {
                 escolhido = p;
             }
         }
-    
+
         System.out.println("");
-        System.out.println("Amigo do Pablo:");
+        System.out.println("Amigo do Pablo: ");
         System.out.println(escolhido.getNome());
     }
-    
+
     public static class Participante {
         private String nome;
         private boolean amigo;
         private int ordemInscricao;
-        
+
         public String getNome() {
             return nome;
         }
-        
+
         public void setNome(String nome) {
             this.nome = nome;
         }
-        
+
         public boolean isAmigo() {
             return amigo;
         }
-        
+
         public void setAmigo(boolean amigo) {
             this.amigo = amigo;
         }
-        
+
         public int getOrdemInscricao() {
             return ordemInscricao;
         }
-        
+
         public void setOrdemInscricao(int ordemInscricao) {
             this.ordemInscricao = ordemInscricao;
         }
-        
+
         @Override
         public String toString() {
             return nome;
